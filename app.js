5b4c5753a05ec3f8fc2641dcc4b4b007694d9665
KarenBoshra <87731739+KarenBoshra@users.noreply.github.com>
2022-08-27 20:00:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-
-/**
+**
  * Define Global Variables
 */
 const navigation = document.getElementById('navbar__list');
@@ -10,37 +9,47 @@ const section =document.querySelectorAll('section');
 */
 
 // build the nav
-function navBuilder() {
+const docFragment = document.createDocumentFragment();
   section.forEach(function (section) {
     listItem = document.createElement("li");
     listItem.innerHTML = `<li> <a a href = "#${section.id}" data-nav= "${section.id}" class="menu__link">${section.dataset.nav}</a>`;
-    navigation.appendChild(listItem);
-  })
-};
-navBuilder();
+    docFragment.appendChild(listItem);
+  });
+  navigation.appendChild(docFragment);
+
+
 
 // Add class 'active' to section when near top of viewport
 // The section will be active when it is viewed by the user
-window.onscroll = function () {
-  document.querySelectorAll("section").forEach(function (active) {
-    const rect = active.getBoundingClientRect();
-    if (
-      rect.top >= -150 &&
-      rect.top <= 150
-    ){
-       active.classList.add("your-active-class");
-    } else {
-      active.classList.remove("your-active-class");
-    }
-  })
+
+const options = {
+  root: null,
+  threshold: 0,
+  rootMargin: "-150px 0px 0px 150px"
 };
 
-// scroll to the apropriate section smoothly after clicking on the nav bar 
-navigation.addEventListener("click", (event) => {
-  event.preventDefault();
-   const section = document.getElementById(`${event.target.dataset.nav}`)
-    section.scrollIntoView({ behavior: "smooth" });
+  const observer = new IntersectionObserver(function (entries, observer) {
+    section.forEach(active=> {
+      if (!active.isIntersecting) {
+        active.classList.add("your-active-class");
+      } else {
+       active.classList.remove("your-active-class");
+
+      }
+    })
+  }, options);
+
+section.forEach(section => {
+  observer.observe(section);
 });
 
+  
+// scroll to the apropriate section smoothly after clicking on the nav bar 
+function scrollToSection(event) {
+  event.preventDefault();
+  const section = document.getElementById(`${event.target.dataset.nav}`)
+  section.scrollIntoView({ behavior: "smooth", block: "center" });
+};
+navigation.addEventListener("click", scrollToSection);
 
 
